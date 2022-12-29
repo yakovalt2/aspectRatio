81088990a4cd9156884272f8707df5c66a1c00f5
yakovalt2 <yakovalt2@gmail.com>
2022-12-29 21:22:17 +0200

    Update Script.js
    Changes in script.js

--- Script.js
+++ Script.js
@@ -1,19 +1,29 @@
 
-var ratiowidth = document.getElementById('ratio-width')
-var ratioheight = document.getElementById('ratio-height')
-var width = document.getElementById('width')
-var height = document.getElementById('height')
+var ratiowidth = document.getElementById('ratio-width');
+var ratioheight = document.getElementById('ratio-height');
+var width = document.getElementById('width');
+var height = document.getElementById('height');
 
+function avoidMinus() {
+    if (ratiowidth.value < 1) ratiowidth.value = 1;
+    if (ratioheight.value < 1) ratioheight.value = 1;
+    if (height.value < 0) height.value = 0;
+    if (width.value < 0) width.value = 0;
+}
 
-ratiowidth.addEventListener('input', function() {
+ratiowidth.addEventListener('input', () => {
+    avoidMinus()
     width.value = height.value/ratioheight.value*ratiowidth.value
 })
-ratioheight.addEventListener('input', function() {
+ratioheight.addEventListener('input', () => {
+    avoidMinus()
     height.value = width.value/ratiowidth.value*ratioheight.value
 })
-width.addEventListener('input', (e) => {
+width.addEventListener('input', () => {
+    avoidMinus()
     height.value = ratioheight.value/ratiowidth.value*width.value
 })
-height.addEventListener('input', (e) => {
+height.addEventListener('input', () => {
+    avoidMinus()
     width.value = ratiowidth.value/ratioheight.value*height.value
 })
